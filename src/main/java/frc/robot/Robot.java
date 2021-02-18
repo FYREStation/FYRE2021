e150fe80f4e25f63e11a33d8850e783ec34c4f0a
Tyler Du <vicguin65@gmail.com>
2021-02-17 21:53:13 -0800

    Code prepared for testing day
    DriveStraight is ready for testing day and every other method needs to be readjusted to correct values when testing day happens

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -167,40 +167,40 @@ public class Robot extends TimedRobot {
   //All of these values are currently correct.
   public void driveBarrel(DriveStraight drivingObject){
     drivingObject.driveStraightDistance(90, true, 0.5);
-    drivingObject.driveArc(30, -354.4413274, true, 0.5);
+    drivingObject.driveArc(30, -354.4413274, true);
     drivingObject.driveStraightDistance(95.39, true, 0.5);
-    drivingObject.driveArc(21, 309.4413274, true, 0.5);
+    drivingObject.driveArc(21, 309.4413274, true);
     drivingObject.driveStraightDistance(84.85, true, 0.5);
-    drivingObject.driveArc(21, 225, true, 0.5);
+    drivingObject.driveArc(21, 225, true);
     drivingObject.driveStraightDistance(240, true, 0.5);
   }
   
   public void driveSlalom(DriveStraight drivingObject){
     drivingObject.driveArc(30, 90, true);
     drivingObject.driveArc(30, -90, true);
-    drivingObject.driveStraightDistance(120, true);
+    drivingObject.driveStraightDistance(120, true, 0.5);
     drivingObject.driveArc(30, -90, true);
     drivingObject.driveArc(30, 360, true);
     drivingObject.driveArc(30, -90, true);
-    drivingObject.driveStraightDistance(120, true);
+    drivingObject.driveStraightDistance(120, true, 0.5);
     drivingObject.driveArc(30, -90, true);
     drivingObject.driveArc(30, 90, true);
   }
 
   public void driveBounce(DriveStraight drivingObject){
     drivingObject.driveArc(30, 90, true);
-    drivingObject.driveStraightDistance(30, true);
-    drivingObject.driveStraightDistance(30, false);
+    drivingObject.driveStraightDistance(30, true, 0.5);
+    drivingObject.driveStraightDistance(30, false, 0.5);
     drivingObject.driveArc(30, 26.57, false);
-    drivingObject.driveStraightDistance(67.08, false);
+    drivingObject.driveStraightDistance(67.08, false, 0.5);
     drivingObject.driveArc(30, 153.43, false);
-    drivingObject.driveStraightDistance(90, false);    
-    drivingObject.driveStraightDistance(90, true);
+    drivingObject.driveStraightDistance(90, false, 0.5);    
+    drivingObject.driveStraightDistance(90, true, 0.5);
     drivingObject.driveArc(30, 90, true);    
-    drivingObject.driveStraightDistance(30, true);
+    drivingObject.driveStraightDistance(30, true, 0.5);
     drivingObject.driveArc(30, 90, true);
-    drivingObject.driveStraightDistance(90, true);
-    drivingObject.driveStraightDistance(30, false);
+    drivingObject.driveStraightDistance(90, true, 0.5);
+    drivingObject.driveStraightDistance(30, false, 0.5);
     drivingObject.driveArc(30, 90, false);
   }
 }
